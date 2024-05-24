c996e5d36480cfeff6660c8463fba2028f5eaed3
June-Ruth <n-barral@hotmail.fr>
2024-05-24 17:45:12 +0200

    add AuthenticationManager

--- src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.training.security.configuration;
 
 import com.training.security.entity.RoleType;
+import com.training.security.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration {
+
+    private CustomUserDetailsService customUserDetailsService;
+
+    public SpringSecurityConfiguration(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,5 +39,13 @@ public class SpringSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return new ProviderManager(authenticationProvider);
     }
 }
