9ba7e0760c063fe9cc526efaeb258878dbd72fd6
June-Ruth <n-barral@hotmail.fr>
2024-05-24 16:03:03 +0200

    create role constant and in memory users

--- src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
@@ -1,10 +1,17 @@
 package com.training.security.configuration;
 
+import com.training.security.entity.RoleType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.core.userdetails.User.builder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +20,27 @@ public class SpringSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = builder()
+                .username("admin")
+                .password(passwordEncoder().encode("test"))
+                .roles(RoleType.ADMIN, RoleType.EDITOR, RoleType.USER).build();
+        UserDetails editor = builder()
+                .username("editor")
+                .password(passwordEncoder().encode("test"))
+                .roles(RoleType.EDITOR, RoleType.USER).build();
+        UserDetails user = builder()
+                .username("user")
+                .password(passwordEncoder().encode("test"))
+                .roles(RoleType.USER).build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/training/security/entity/RoleType.java
@@ -0,0 +1,9 @@
+package com.training.security.entity;
+
+public final class RoleType {
+
+    public static final String ADMIN = "ADMIN";
+    public static final String EDITOR = "EDITOR";
+    public static final String USER= "USER";
+
+}
