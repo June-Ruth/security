2d7c373facbd7d420c980e89b1e6c9bfa41ec075
June-Ruth <n-barral@hotmail.fr>
2024-05-24 15:24:39 +0200

    create LoginController to welcome each role

---
+++ src/main/java/com/training/security/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.training.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/admin")
+    public String welcomeAdmin() {
+        return "Welcome Admin";
+    }
+
+    @GetMapping("/editor")
+    public String welcomeEditor() {
+        return "Welcome Editor";
+    }
+
+    @GetMapping("/user")
+    public String welcomeUser() {
+        return "Welcome User";
+    }
+
+}
