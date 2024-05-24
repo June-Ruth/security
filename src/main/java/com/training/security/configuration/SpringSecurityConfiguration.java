382d5d9e0586ee1474e4f654c3b0f19694f15563
June-Ruth <n-barral@hotmail.fr>
2024-05-24 16:25:26 +0200

    add role filtering

--- src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.training.security.configuration;
 import com.training.security.entity.RoleType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,7 +20,15 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin").hasRole(RoleType.ADMIN)
+                        .requestMatchers("/editor").hasRole(RoleType.EDITOR)
+                        .requestMatchers("/user").hasRole(RoleType.USER)
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
