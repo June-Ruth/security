f3bf211405230d899814420de6689b674c7ea2a4
June-Ruth <n-barral@hotmail.fr>
2024-05-26 21:03:45 +0200

    look github token

--- src/main/java/com/training/security/controller/LoginController.java
+++ src/main/java/com/training/security/controller/LoginController.java
@@ -35,7 +35,7 @@ public class LoginController {
 
     @GetMapping("/")
     public String getGitHub(Principal user) {
-        return "Welcome " + user.getName();
+        return "Welcome " + user.toString();
     }
 
 }
