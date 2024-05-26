4d0580c0332bedd5295b3610672e5308f258a1ee
June-Ruth <n-barral@hotmail.fr>
2024-05-26 15:44:00 +0200

    add PrivilegeType

---
+++ src/main/java/com/training/security/entity/PrivilegeType.java
@@ -0,0 +1,9 @@
+package com.training.security.entity;
+
+public final class PrivilegeType {
+
+    public static final String READ = "READ";
+    public static final String WRITE = "WRITE";
+    public static final String DELETE = "DELETE";
+
+}
