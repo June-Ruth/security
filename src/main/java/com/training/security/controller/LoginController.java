fb6675bfc6927c355a976f29d2178d8cf8f3f676
June-Ruth <n-barral@hotmail.fr>
2024-05-26 21:25:05 +0200

    add OpenId connect and get Claims

--- src/main/java/com/training/security/controller/LoginController.java
+++ src/main/java/com/training/security/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.training.security.controller;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,12 +44,12 @@ public class LoginController {
     }
 
     @GetMapping("/default")
-    public String getUserInfo(Principal user) {
+    public String getUserInfo(Principal user, @AuthenticationPrincipal OidcUser oidcUser) {
         StringBuffer userInfo = new StringBuffer();
         if (user instanceof UsernamePasswordAuthenticationToken) {
             userInfo.append(getUsernamePasswordLoginInfo(user));
         } else if (user instanceof OAuth2AuthenticationToken) {
-            userInfo.append(getOauth2LoginInfo(user));
+            userInfo.append(getOauth2LoginInfo(user, oidcUser));
         }
         return userInfo.toString();
     }
@@ -66,7 +69,7 @@ public class LoginController {
         return usernameInfo;
     }
 
-    private StringBuffer getOauth2LoginInfo(Principal user) {
+    private StringBuffer getOauth2LoginInfo(Principal user, OidcUser oidcUser) {
         StringBuffer protectedInfo = new StringBuffer();
 
         OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
@@ -75,6 +78,18 @@ public class LoginController {
             Map<String, Object> userAttributes = authToken.getPrincipal().getAttributes();
             protectedInfo.append("Welcome ").append(userAttributes.get("name")).append("<br><br>");
             protectedInfo.append("e-mail: ").append(userAttributes.get("email")).append("<br><br>");
+
+            if (oidcUser != null) {
+                OidcIdToken idToken = oidcUser.getIdToken();
+                if (idToken != null) {
+                    protectedInfo.append("Token mapped values <br><br>");
+                    Map<String, Object> claims = idToken.getClaims();
+                    for (String key : claims.keySet()) {
+                        protectedInfo.append("  ").append(key).append(": ").append(claims.get(key)).append("<br>");
+                    }
+                }
+            }
+
         } else {
             protectedInfo.append("NA");
         }
