99829c2fc68fd31a11f13c666d8fb11c94a7ccf4
June-Ruth <n-barral@hotmail.fr>
2024-05-26 20:47:42 +0200

    add privilege test

--- src/test/java/com/training/security/controller/PrivilegeControllerTest.java
+++ src/test/java/com/training/security/controller/PrivilegeControllerTest.java
@@ -1,2 +1,55 @@
-package com.training.security.controller;public class PrivilegeControllerTest {
+package com.training.security.controller;
+
+import com.training.security.entity.PrivilegeType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PrivilegeControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+    /*
+        GET : /read
+        - With : READ_PRIVILEGE -> expect : status OK
+     */
+    @Test
+    @WithMockUser(authorities = PrivilegeType.READ)
+    void AsReadWhenGetReadThenExpectStatus200() throws Exception {
+        mvc.perform(get("/read"))
+                .andExpect(status().isOk());
+    }
+
+    /*
+        GET : /read
+        - With : WRITE_PRIVILEGE -> expect : status 403
+     */
+    @Test
+    @WithMockUser(roles = PrivilegeType.WRITE)
+    void AsWriteWhenGetReadThenExpectStatu403() throws Exception {
+        mvc.perform(get("/read"))
+                .andExpect(status().isForbidden());
+    }
+
+    /*
+        GET : /read
+        - With : DELETE_PRIVILEGE -> expect : status 403
+     */
+    @Test
+    @WithMockUser(authorities = PrivilegeType.DELETE)
+    void AsDeleteWhenGetReadThenExpectStatus403() throws Exception {
+        mvc.perform(get("/read"))
+                .andExpect(status().isForbidden());
+    }
+
 }
