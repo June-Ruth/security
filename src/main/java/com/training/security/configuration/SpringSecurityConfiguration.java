8b98372337c4b52e8e9da63294f6a265246e2dea
June-Ruth <n-barral@hotmail.fr>
2024-05-26 21:35:32 +0200

    remember me always

--- src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/training/security/configuration/SpringSecurityConfiguration.java
@@ -52,7 +52,8 @@ public class SpringSecurityConfiguration {
                         .defaultSuccessUrl("/default"))
                 .logout(logout -> logout
                         .logoutSuccessUrl("/home"))
-                .rememberMe(Customizer.withDefaults())
+                .rememberMe(remember -> remember
+                        .alwaysRemember(true))
                 .build();
     }
 
